e306999e592cc0a1e82a3493e3950d21ff3d14eb
Sebastian Kramer <sebastian.kramer@wp.pl>
2016-12-20 23:17:53 +0100

    string interpolators

--- src/test/scala/org/skramer/scalalang/BasicTypesAndOperatorsTest.scala
+++ src/test/scala/org/skramer/scalalang/BasicTypesAndOperatorsTest.scala
@@ -16,4 +16,16 @@ class BasicTypesAndOperatorsTest extends FlatSpecWithMatchers {
     symbol should be theSameInstanceAs Symbol("symbol")
   }
 
+  "raw interpolator" should "ignore escape signs" in {
+    raw"\\\\" should have length 4
+  }
+
+  "f interpolator" should "default to string interpolator" in {
+    val h = "hello"
+    f"$h" should have length 5
+  }
+
+  "f interpolator" should "use printf syntax" in {
+    f"${math.Pi}%.5f" should fullyMatch regex (raw"(\d)\.(\d+)" withGroups("3", "14159"))
+  }
 }
