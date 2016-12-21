135fde43ab952cf4f3f797218765610175dbb1d9
Sebastian Kramer <sebastian.kramer@wp.pl>
2016-12-21 15:14:09 +0100

    rationals can now be added to each other

--- src/main/scala/org/skramer/scalalang/rational/Rational.scala
+++ src/main/scala/org/skramer/scalalang/rational/Rational.scala
@@ -3,8 +3,12 @@ package org.skramer.scalalang.rational
 /**
   * Created by skramer on 21.12.16.
   */
-class Rational(nominator: Int, denominator: Int) {
+class Rational(val nominator: Int, val denominator: Int) {
   require(denominator != 0)
 
   override def toString: String = s"$nominator / $denominator"
+
+  def add(other: Rational): Rational = {
+    new Rational(nominator * other.denominator + other.nominator * denominator, denominator * other.denominator)
+  }
 }
