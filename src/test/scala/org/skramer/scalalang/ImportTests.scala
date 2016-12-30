664391a41d0086b930e742e9a9ce437a9dc81248
Sebastian Kramer <sebastian.kramer@wp.pl>
2016-12-30 19:27:17 +0100

    rename import of members of an instance

--- src/test/scala/org/skramer/scalalang/ImportTests.scala
+++ src/test/scala/org/skramer/scalalang/ImportTests.scala
@@ -1,16 +1,17 @@
 package org.skramer.scalalang
 
+
+class Foo {
+  def bar: Int = 5
+
+  def baz: Int = 15
+}
+
 /**
   * Created by skramer on 30.12.16.
   */
-
 class ImportTests extends FlatSpecWithMatchers {
   "all members of a class" can "be imported" in {
-    class Foo {
-      def bar: Int = 5
-
-      def baz: Int = 15
-    }
 
     def checkFoo(f: Foo): Unit = {
       import f._
@@ -19,5 +20,13 @@ class ImportTests extends FlatSpecWithMatchers {
     }
 
     checkFoo(new Foo)
+  }
+
+  "import" can "rename member" in {
+    def checkFoo(f: Foo) = {
+      import f.{bar => first, baz => second}
+      first shouldBe 5
+      second shouldBe 15
+    }
   }
 }
