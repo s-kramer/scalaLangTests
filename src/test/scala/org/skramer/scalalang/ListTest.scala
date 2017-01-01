f1494aacbdd48b78014954c3b0ae4293be554467
Sebastian Kramer <sebastian.kramer@wp.pl>
2017-01-01 17:56:25 +0100

    list covariance

---
+++ src/test/scala/org/skramer/scalalang/ListTest.scala
@@ -0,0 +1,13 @@
+package org.skramer.scalalang
+
+/**
+  * Created by skramer on 01.01.17.
+  */
+class ListTest extends FlatSpecWithMatchers {
+  "list" should "be covariant" in {
+    val listOfInt = List(1, 2, 3, 4)
+    val listOfAny: List[Any] = listOfInt
+
+    listOfAny should be(a[List[_]])
+  }
+}
