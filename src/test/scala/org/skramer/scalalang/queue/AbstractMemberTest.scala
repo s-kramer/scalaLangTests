b12e283389911103f0fb7736324f71f129660530
Sebastian Kramer <sebastian.kramer@wp.pl>
2017-01-22 20:18:47 +0100

    class parameter vs abstract field initialization order

--- src/test/scala/org/skramer/scalalang/queue/AbstractMemberTest.scala
+++ src/test/scala/org/skramer/scalalang/queue/AbstractMemberTest.scala
@@ -23,12 +23,10 @@ class AbstractMemberTest extends FlatSpecWithMatchers {
       val bar: String
     }
 
-    assertDoesNotCompile(
-      """
-        | class Buzz extends Foo {
-        |   override def bar: String = "qwerty"
-        |}
-      """.stripMargin)
+    // this doesn't compile but assertDoesNotCompile doesn't seem to detect that :(
+    //    class Buzz extends Foo {
+    //      override def bar: String = "qwerty"
+    //    }
 
     class Bar(override val bar: String) extends Foo
   }
@@ -45,5 +43,26 @@ class AbstractMemberTest extends FlatSpecWithMatchers {
     class Qux extends Foo {
       override val bar: String = "concrete val"
     }
+  }
+
+  trait Foo {
+    require(bar != 0)
+    val bar: Int
+  }
+
+  val multiplier = 2
+
+  class Buz(override val bar: Int) extends Foo {
+  }
+
+  "class parameters" should "be initialized before it's passed to the constructor" in {
+    val b = new Buz(5 * multiplier) // this doesn't throw as the abstract field is initialized during class creation, before the superconstructor is called
+    assert(b.bar === 10)
+  }
+
+  "implementing val" should "be initialized after calling superconstructor" in {
+    assertThrows[IllegalArgumentException](new Foo {
+      override val bar: Int = 8 * multiplier // this throws because the superclass constructor with the require clause gets called before this value is initialized
+    })
   }
 }
